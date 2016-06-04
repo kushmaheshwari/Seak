6273d512e2409fb1265ccc9d8417f13583d5acbe
Roman <email@romanvolkov.ru>
2016-06-04 14:34:31 +0500

    user login refactoring

---
+++ SeakApp/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  SeakApp
+//
+//  Created by Roman Volkov on 04/06/16.
+//  Copyright © 2016 Kush Maheshwari. All rights reserved.
+//
+
+import Foundation
---
+++ SeakApp/UserLogin.swift
@@ -0,0 +1,9 @@
+//
+//  UserLogin.swift
+//  SeakApp
+//
+//  Created by Roman Volkov on 04/06/16.
+//  Copyright © 2016 Kush Maheshwari. All rights reserved.
+//
+
+import Foundation
