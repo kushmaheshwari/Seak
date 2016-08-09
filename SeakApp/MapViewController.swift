f3582342b17d40bce8fb8199cc25588f1c89dc36
Roman Volkov <email@romanvolkov.ru>
2016-08-09 20:57:13 +0200

    Developed search on SetLocation page

--- SeakApp/MapViewController.swift
+++ SeakApp/MapViewController.swift
@@ -15,7 +15,9 @@ class MapViewController: UIViewController,
                          MKMapViewDelegate,
                          CLLocationManagerDelegate,
                          UISearchBarDelegate,
-                         UITableViewDelegate
+                         UITableViewDelegate,
+                         UITableViewDataSource,
+                         UITextFieldDelegate
 {
     var resultSearchController: UISearchController? = nil
     @IBOutlet weak var searchBar: UISearchBar!
@@ -26,22 +28,73 @@ class MapViewController: UIViewController,
     private let locationManager = CLLocationManager()
     private var currentLocation: CLLocationCoordinate2D? = nil
     @IBOutlet weak var locationButton: UIButton!
+    var mapSearchItems: [MKMapItem]? = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "Set Location"
-        self.mapView.delegate = self
         
         locationButton.layer.cornerRadius = 15
         
+        self.mapView.delegate = self
         if let coordinates = UserDataCache.getUserLocation() {
             self.mapView.centerCoordinate = coordinates
             self.mapView.region = MKCoordinateRegionMakeWithDistance(coordinates, 700, 700)
         }
         
         searchResultTableView.delegate = self
-        searchBar.delegate = self
+        searchResultTableView.dataSource = self
         searchResultTableView.hidden = true
+        searchResultTableView.reloadData()
+        searchBar.delegate = self
+        
+        for view: UIView in searchBar.subviews {
+            if (view is UITextField) {
+                let tf: UITextField = (view as! UITextField)
+                tf.delegate = self
+            }
+        }
+    }
+    
+    func textFieldShouldClear(textField: UITextField) -> Bool {
+        self.performSelector(#selector(self.searchBarCancelButtonClicked), withObject: self.searchBar, afterDelay: 0.1)
+        return true
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if mapSearchItems?.count >= 4
+        {
+            self.searchResultCount = 4
+        }
+        else
+        {
+            self.searchResultCount = (mapSearchItems?.count)!
+        }
+        return self.searchResultCount
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.searchResultTableView.dequeueReusableCellWithIdentifier("cellID")
+        if mapSearchItems != nil
+        {
+            cell?.textLabel?.text = mapSearchItems![indexPath.row].placemark.title
+        }
+        return cell!
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let selectedItem = mapSearchItems![indexPath.row]
+        self.mapView.centerCoordinate = selectedItem.placemark.coordinate
+        searchResultTableView.hidden = true
+        searchBar.performSelector(#selector(self.resignFirstResponder), withObject: nil, afterDelay: 0.1)
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 30
     }
     
     @IBAction func saveLocation(sender: AnyObject) {
@@ -50,13 +103,14 @@ class MapViewController: UIViewController,
         UserDataCache.saveUserLocationLong(currentLocation?.longitude)
     }
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return searchResultCount
-    }
     
-    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
-        searchResultTableView.hidden = false
-       
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.characters.count == 0 {
+            searchBar.performSelector(#selector(self.resignFirstResponder), withObject: nil, afterDelay: 0.1)
+            searchResultTableView.hidden = true
+        }
+
+        
         if self.mapView.annotations.count != 0
         {
             let annotation = self.mapView.annotations[0]
@@ -64,22 +118,24 @@ class MapViewController: UIViewController,
         }
         
         let localSearchRequest = MKLocalSearchRequest()
-        localSearchRequest.naturalLanguageQuery = searchBar.text
+        localSearchRequest.naturalLanguageQuery = searchText
         let localSearch = MKLocalSearch(request: localSearchRequest)
         localSearch.startWithCompletionHandler { (localSearchResponse, error) -> Void in
             if localSearchResponse != nil
             {
-                if localSearchResponse?.mapItems.count >= 4
-                {
-                    self.searchResultCount = 4
+                self.mapSearchItems = localSearchResponse?.mapItems
+                
+                self.searchResultTableView.reloadData()
+                var tableHeight: CGFloat = 0.0
+                let len = (self.mapSearchItems!.count>=4) ? 4 : self.mapSearchItems!.count
+                for i in 0..<len {
+                    tableHeight += self.tableView(self.searchResultTableView, heightForRowAtIndexPath: NSIndexPath(forRow: i, inSection: 0))
                 }
-                else
-                {
-                    self.searchResultCount = (localSearchResponse?.mapItems.count)!
-                }
-                self.mapView.centerCoordinate = CLLocationCoordinate2D(latitude: localSearchResponse!.boundingRegion.center.latitude, longitude: localSearchResponse!.boundingRegion.center.longitude)
+                self.searchResultTableView.frame = CGRectMake(self.searchResultTableView.frame.origin.x, self.searchResultTableView.frame.origin.y, self.searchResultTableView.frame.size.width, tableHeight)
+                self.searchResultTableView.hidden = false
             }
         }
+
     }
     
 }
