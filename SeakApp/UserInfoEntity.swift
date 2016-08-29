ad2965139a413d171fafab16bd5029311ed00abd
Roman Volkov <email@romanvolkov.ru>
2016-08-29 14:19:42 +0200

    Created UserInfoEntity and UserRepository

---
+++ SeakApp/UserInfoEntity.swift
@@ -0,0 +1,15 @@
+//
+//  UserInfoEntity.swift
+//  SeakApp
+//
+//  Created by Екатерина Волкова on 29/08/16.
+//  Copyright © 2016 Kush Maheshwari. All rights reserved.
+//
+
+import Foundation
+
+class UserInfoItem {
+    var userId: String?
+    var userEmail: String?
+    var picutre: String?
+}
---
+++ SeakApp/UserRepository.swift
@@ -0,0 +1,9 @@
+//
+//  UserRepository.swift
+//  SeakApp
+//
+//  Created by Екатерина Волкова on 29/08/16.
+//  Copyright © 2016 Kush Maheshwari. All rights reserved.
+//
+
+import Foundation
