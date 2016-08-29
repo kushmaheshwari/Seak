d6006d960549c22f66e8d5d16452d7fae418a5da
Roman Volkov <email@romanvolkov.ru>
2016-08-29 14:50:29 +0200

    Created method processUser in UserRepository

--- SeakApp/UserRepository.swift
+++ SeakApp/UserRepository.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+import Firebase
+
+//method parse
+//method by user id
+// save user
+
+class UserRepository {
+    
+    static func processUser(userId: String?, userObject: [String: AnyObject]) -> UserInfoItem
+    {
+        let userInfoEntity = UserInfoItem()
+        userInfoEntity.userId = userId
+        
+        if let userEmail = userObject["userEmail"] as? String {
+            userInfoEntity.userEmail = userEmail
+        }
+        
+        if let userPic = userObject["picutre"] as? String {
+            userInfoEntity.picutre = userPic
+        }
+        
+        return userInfoEntity
+    }
+}
